c14cc2c86f14023323d8aa6407f700ffdb48ffda
Joseph B <mcjoseph789@gmail.com>
2021-10-08 09:44:07 -0500

    tabla html basica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Articulo } from './articulo.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto-directivas';
+  title = 'Administración de artículos';
+  codigoInput = 0.0;
+  descripcionInput = '';
+  precioInput = 0.0;
+
+  articulos: Articulo[] = [
+    new Articulo(1,'papas',10.55),
+    new Articulo(2,'manzanas',12.1),
+    new Articulo(3,'melón',52.3),
+    new Articulo(4,'cebollas',17),
+    new Articulo(5,'calabaza',20)
+  ]
+
+  agregarArticulo(){
+    let artaux = new Articulo(this.codigoInput, this.descripcionInput, this.precioInput);
+    this.articulos.push(artaux);
+  }
 }
